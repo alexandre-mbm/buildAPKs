f5378ffc236f7101605208b463dc7c2baea62341
Alexandre Magno <alexandre.mbm@gmail.com>
2019-09-24 09:51:13 -0300

    .gitmodules

--- scripts/bash/tdebs/fix.ecj.error.bash
+++ scripts/bash/tdebs/fix.ecj.error.bash
@@ -36,10 +36,11 @@ trap _SDGTRPSIGNAL_ HUP INT TERM
 trap _SDGTRPQUIT_ QUIT 
 export RDR="$HOME/buildAPKs/"
 cd "$RDR"
-(git submodule add https://github.com/BuildAPKs/debs.ecj.buildAPKs ./debs/ecj4.7) || (printf "\\n\\n%s\\n" "Cannot update: continuing...") # https://www.tecmint.com/chaining-operators-in-linux-with-practical-examples/
+(git submodule update --init --recursive --remote debs/ecj4.7) || (printf "\\n\\n%s\\n" "Cannot update: continuing...") # https://www.tecmint.com/chaining-operators-in-linux-with-practical-examples/
 dpkg --purge ecj ecj4.6
 dpkg --install "$RDR/debs/ecj4.7/ecj_4.7.2-1_all.deb"
 rm -f "$RDR/tmp/*err"
 echo "Error repaired!"
+echo
 
 #EOF
